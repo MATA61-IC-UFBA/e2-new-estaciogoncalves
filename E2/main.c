14cf7316593a37c24707dc342e83d8c298012952
estaciogoncalves <124896156+estaciogoncalves@users.noreply.github.com>
2024-08-09 06:55:54 -0400

    Update main.c

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,26 @@
 /* main.c */
-
+int main(argc, argv)
+int argc;
+char **argv;
+{
+  if(argc > 1) {
+    if(!(yyin = fopen(argv[1], "r"))) {
+      perror(argv[1]);
+      return (1);
+    }
+  }
+  int tok = yylex();
+  while(1) {
+    if(tok == NUMBER) {
+      printf(" constante decimal: %d\n", yylval);
+    }
+    else { 
+      printf(" código do token: %d\n", tok);
+    }
+    tok = yylex();
+    if(tok == EOL){
+      printf(" código do token: %d\n", tok);
+      break;
+    }
+  }
+}
